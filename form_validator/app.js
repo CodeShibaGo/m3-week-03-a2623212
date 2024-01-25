946a85b248fd00ff1e699f1893c51d539cb0a163
a2623212 <a2623212@gmail.com>
2024-01-25 10:37:19 +0900

    feat: add username validation check

--- form_validator/app.js
+++ form_validator/app.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function () {
+  $("button").click(function (e) {
+    e.preventDefault();
+    const username = $("#username").val().trim();
+    const email = $("#email").val().trim();
+    const password = $("#password").val().trim();
+    const password2 = $("#password2").val().trim();
+    // Username
+    if (username.length < 3) {
+      $("#username").parent().addClass("error");
+      $("#username")
+        .parent()
+        .find("small")
+        .text("Username must be at least 3 characters");
+    } else {
+      $("#username").parent().addClass("success");
+    }
+    //Email
+  });
+});
